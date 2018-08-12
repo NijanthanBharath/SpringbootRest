6035de9490009a791beebf0c1ae518585428b154
NijanthanBharath <bharathnijanthan2@gmail.com>
2018-08-12 14:21:45 +0530

    Initial commit

---
+++ test/src/main/java/com/nijanthan/TestController.java
@@ -0,0 +1,55 @@
+package com.nijanthan;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "test")
+public class TestController {
+
+	@RequestMapping(value = "/getHello", method = RequestMethod.GET)
+	public String getHello() {
+		//http://localhost:12345/test/getHello
+		return "Hello";
+	}
+
+	@RequestMapping(value = "/getHelloPath/{world}", method = RequestMethod.GET)
+	public String getHelloPath(@PathVariable
+			("world") String world) {
+		//http://localhost:12345/test/getHelloPath/nijanthanWorld
+		return "Hello path " + world;
+	}
+	
+	@RequestMapping(value = "/getHelloQuery", method = RequestMethod.GET)
+	public String getHelloQuery(@RequestParam("world") String world) {
+		//http://localhost:12345/test/getHelloQuery?world=nijanthanWorld
+		return "Hello query " + world;
+	}
+	
+	@RequestMapping(value = "/getHelloBody/{mark}", method = RequestMethod.POST)
+	public TestDto getHelloBody(@RequestParam("name") String name,@PathVariable("mark") long mark) {
+		//http://localhost:12345/test/getHelloBody/12?name=Nijanthan
+		TestDto dto=new TestDto();
+		dto.setName(name);
+		dto.setMark(mark);
+		return dto;
+	}
+	
+	@RequestMapping(value = "/getHelloRequestBody/{mark}", method = RequestMethod.POST)
+	public TestDto getHelloBody(@RequestBody TestDto requestDto,@PathVariable("mark") long mark) {
+		//http://localhost:12345/test/getHelloRequestBody/12
+		//{	    "name": "Nijanthan",	    "mark": 12	}
+		TestDto dto=new TestDto();
+		dto.setName(requestDto.getName());
+		dto.setMark(requestDto.getMark()+mark);
+		return dto;
+	}
+	
+
+}
---
+++ test/src/main/java/com/nijanthan/TestDto.java
@@ -0,0 +1,26 @@
+package com.nijanthan;
+
+public class TestDto {
+	
+	private String name;
+	
+	private long mark;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getMark() {
+		return mark;
+	}
+
+	public void setMark(long mark) {
+		this.mark = mark;
+	}
+	
+
+}
